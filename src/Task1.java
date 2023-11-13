72ca9f1fb21a977e229d7d3507713b4a6da2ed45
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-11-13 21:18:09 +0100

    Add starting methods

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task1 {
+public class Task1 {
+
+    public static void main(String[] args){
+
+    }
+    public int[] readNumbers(){
+        int[] numbers = new int[5];
+        return numbers;
+    }
+    public void quickSortIterative(int[] numbers, int size){
+
+    }
+    public void quickSortRecursive(int[] numbers){
+
+    }
+    public void insertionSortIterative(int[] numbers){
+
+    }
+    public void insertionSortRecursive(int[] numbers){
+        
+    }
 }
